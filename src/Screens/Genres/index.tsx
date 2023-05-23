d32db826d8a18e4c9b0101464282a2a205f04a35
Your Name <you@example.com>
2023-05-23 20:25:01 -0300

    fazendo botoes de generos

--- src/Screens/Genres/index.tsx
+++ src/Screens/Genres/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {Button, Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-// import AsyncStorage from '@react-native-community/async-storage';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 export function Genres({navigation}: any) {
+  const genres: any = [];
+
   function openScreen() {
     navigation.navigate('Movie');
   }
@@ -21,8 +23,49 @@ export function Genres({navigation}: any) {
           Selecione seus <Text style={{color: '#B43239'}}>gêneros</Text>{' '}
           favoritos
         </Text>
-        <Button title="Filmes" onPress={openScreen} />
       </View>
+      <View style={styles.genresArea}>
+        <View style={styles.genresRow}>
+          <TouchableOpacity
+            style={styles.buttomGenre}
+            onPress={() => genres.push('drama')}>
+            <Text style={styles.textGenre}>Drama</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Ação</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.genresRow}>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Comédia</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Terror</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.genresRow}>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Fic científica</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Romance</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.genresRow}>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Suspense</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttomGenre}>
+            <Text style={styles.textGenre}>Animação</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.buttomContinue}
+        onPress={() => console.log(genres)}>
+        <Text style={styles.textBtnCon}>Avançar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -31,7 +74,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     backgroundColor: '#000',
     padding: 20,
     height: '100%',
@@ -41,12 +84,37 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 25,
   },
+  textBtnCon: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  textGenre: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
   headerArea: {
     width: '70%',
     height: '12%',
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 15,
+    // backgroundColor: '#f1f',
+  },
+  genresArea: {
+    width: '70%',
+    height: '60%',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // backgroundColor: '#f00',
+  },
+  genresRow: {
+    width: '100%',
+    height: '12%',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    // backgroundColor: '#f1f',
+    marginBottom: '2%',
+    flexDirection: 'row',
   },
   buttomBack: {
     backgroundColor: '#B43239',
@@ -55,5 +123,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 6,
+  },
+  buttomContinue: {
+    backgroundColor: '#B43239',
+    width: '40%',
+    height: '7%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+  },
+  buttomGenre: {
+    backgroundColor: '#fff',
+    width: '45%',
+    height: '75%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
   },
 });
